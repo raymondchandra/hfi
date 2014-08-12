0f52728882edb35c5c3bc6faff08fd03ff80b878
Bernico <bewnico@yahoo.com>
2014-08-12 11:46:50 +0700

    routes

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,67 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	return View::make('pages.home');
 });
+
+Route::get('about', function()
+{
+	return View::make('pages.about');
+}
+);
+
+Route::get('projects', function()
+{
+	return View::make('pages.projects');
+}
+);
+
+Route::get('contact', function()
+{
+	return View::make('pages.contact');
+}
+);
+
+
+Route::get('laravelregistrasianggota',function()
+{
+	return View::make('pages.laravelregistrasianggota');
+}
+);
+
+//route ke halaman registrasi -> redirect ke folder pages, file registrasi.blade.php
+Route::get('registrasianggota',function()
+{
+	return View::make('pages.registrasianggota');
+}
+);
+
+//route ke halaman homeanggota -> redirect ke folder pages, file homeanggota.blade.php
+Route::get('homeanggota', function()
+{
+	return View::make('pages.homeanggota');
+}
+);
+
+
+//route ke halaman daftaranggota -> redirect ke folder pages, file daftaranggota.blade.php
+Route::get('daftaranggota', function()
+{
+	return View::make('pages.daftaranggota');
+}
+);
+
+//route ke halaman ketentuananggota -> redirect ke folder pages, file ketentuananggota.blade.php
+Route::get('ketentuananggota', function()
+{
+	return View::make('pages.ketentuananggota');
+}
+);
+
+/*
+//route ke halaman pascaregistrasi -> redirect ke folder pages, file pascaregistrasi
+Route::get('pascaregistrasi',function()
+{
+	return View::make('pages.pascaregistrasi');
+}
+);*/

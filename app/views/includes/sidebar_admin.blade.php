c7246b7973a04e843c91652d085455b41cdb0344
domiworks <domiworks@gmail.com>
2014-08-14 00:48:17 +0700

    Adminpanel Layout HTML + CSS +JQuery Completed

---
+++ app/views/includes/sidebar_admin.blade.php
@@ -0,0 +1,93 @@
+ <!-- sidebar nav -->
+	<nav class="sidebar_admin">
+		<ul>
+			<li>
+				<a href="#" class="_home_tombol">Home</a>
+				<ul style="display: none;" class="_home_list">
+					<li>
+						<a href="#">Slideshow</a>
+					</li>
+					<li>
+						<a href="#">Welcome Message</a>
+					</li>
+					<li>
+						<a href="#">Tentang HFI</a>
+					</li>
+					<li>
+						<a href="#">Visi</a>
+					</li>
+					<li>
+						<a href="#">Misi</a>
+					</li>
+					<li>
+						<a href="#">Regulasi</a>
+					</li>
+				</ul>
+			</li>
+			
+			<li>
+				<a href="#" class="_organisasi_tombol">Organisasi</a>
+				<ul style="display: none;" class="_organisasi_list">
+					<li>
+						<a href="#">Pengurus</a>
+					</li>
+					<li>
+						<a href="#">Cabang</a>
+					</li>
+				</ul>
+			</li>
+			
+			<li>
+				<a href="#">Kegiatan</a>
+			</li>
+			
+			<li>
+				<a href="#" class="_publikasi_tombol">Publikasi</a>
+				<ul style="display: none;" class="_publikasi_list">
+					<li>
+						<a href="#">Jenis Publikasi</a>
+					</li>
+					<li>
+						<a href="#">Ketentuan Umum</a>
+					</li>
+					<li>
+						<a href="#">Karya Tulis Lain</a>
+					</li>
+					<li>
+						<a href="#">Ilmiah Populer</a>
+					</li>
+				</ul>
+			</li>
+			
+			<li>
+				<a href="#" class="_anggota_tombol">Anggota</a>
+				<ul style="display: none;" class="_anggota_list">
+					<li>
+						<a href="#">Aturan Menjadi Anggota</a>
+					</li>
+					<li>
+						<a href="#">Daftar Anggota</a>
+					</li>
+					<li>
+						<a href="#">Daftar Akun</a>
+					</li>
+				</ul>
+			</li>
+			
+			<li>
+				<a href="#">e-Berkas</a>
+			</li>
+			
+			<li>
+				<a href="#" class="_kontak_tombol">Kontak</a>
+				<ul style="display: none;" class="_kontak_list">
+					<li>
+						<a href="#">Informasi Kontak</a>
+					</li>
+					<li>
+						<a href="#">Email HFI</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
---
+++ app/views/layouts/adminlayout.blade.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html>
+<head>
+	@include('includes.head')
+</head>
+<body>
+<div class="container">
+
+	@include('includes.header_admin')
+	
+
+	<section class="admin_content_container">
+	
+		@include('includes.sidebar_admin')
+		
+		<div class="admin_control_panel">
+			<div class="container_12">
+				@yield('content')
+			</div>
+		</div>
+		
+		
+	</section>
+
+
+</div>
+<script type="text/javascript">
+		
+		$('body').css('overflow','hidden');
+		
+		function updateSize(){
+		
+			var height = $(window).height() - $('.header_admin').height();
+			var width = $(window).width() - $('.admin_logo').width();
+			$('.admin_content_container, .sidebar_admin, .admin_control_panel').css('height',height);
+			$('.admin_control_panel, .admin_id').css('width',width);
+		
+			
+		};
+		$(document).ready(updateSize);
+		$(window).resize(updateSize);
+		
+		/*external container area exit trigger*/
+		$('.exit').click(function() {$( ".pop_up_super_c" ).fadeOut( 200, function(){});});	
+		
+		$('.pop_up_super_c').click(function (e)
+		{
+			var container = $('.pop_up_cell');
+
+			if (container.is(e.target) )// if the target of the click is the container...
+			{
+				$( ".pop_up_super_c" ).fadeOut( 200, function(){});
+				$('html').css('overflow-y', 'auto');
+			}
+		});
+		
+		
+		
+		
+		$('._home_tombol').click(function ()
+		{
+			if ($('._home_list').is(':visible')) {
+				$( "._home_list" ).slideUp( 370, function(){
+					$('._home_list').css('display', 'none');
+				});
+			} else {
+				$( "._home_list" ).slideDown( 370, function(){
+					$('._home_list').css('display', 'block');
+				});
+			}
+		});
+		
+		$('._organisasi_tombol').click(function ()
+		{
+			if ($('._organisasi_list').is(':visible')) {
+				$( "._organisasi_list" ).slideUp( 370, function(){
+					$('._organisasi_list').css('display', 'none');
+				});
+			} else {
+				$( "._organisasi_list" ).slideDown( 370, function(){
+					$('._organisasi_list').css('display', 'block');
+				});
+			}
+		});
+		
+		$('._publikasi_tombol').click(function ()
+		{
+			if ($('._publikasi_list').is(':visible')) {
+				$( "._publikasi_list" ).slideUp( 370, function(){
+					$('._publikasi_list').css('display', 'none');
+				});
+			} else {
+				$( "._publikasi_list" ).slideDown( 370, function(){
+					$('._publikasi_list').css('display', 'block');
+				});
+			}
+		});
+		
+		$('._anggota_tombol').click(function ()
+		{
+			if ($('._anggota_list').is(':visible')) {
+				$( "._anggota_list" ).slideUp( 370, function(){
+					$('._anggota_list').css('display', 'none');
+				});
+			} else {
+				$( "._anggota_list" ).slideDown( 370, function(){
+					$('._anggota_list').css('display', 'block');
+				});
+			}
+		});
+		
+		$('._kontak_tombol').click(function ()
+		{
+			if ($('._kontak_list').is(':visible')) {
+				$( "._kontak_list" ).slideUp( 370, function(){
+					$('._kontak_list').css('display', 'none');
+				});
+			} else {
+				$( "._kontak_list" ).slideDown( 370, function(){
+					$('._kontak_list').css('display', 'block');
+				});
+			}
+		});
+		
+		
+		
+	</script>
+</body>
+</html>

35ffd57d8e875c8e420a38d97ab2f9c6e4f8a5c1
domiworks <domiworks@gmail.com>
2014-08-14 12:28:40 +0700

    Regsitrasi Validator

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -8,5 +8,6 @@
 	
 	<script type="text/javascript" src="assets/js/jquery-1.11.1.min.js"></script>
 	<script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
 
 	

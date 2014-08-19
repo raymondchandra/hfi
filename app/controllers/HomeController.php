ceff6066eb2360461b75e7186bbd28d96c4daf21
domiworks <domiworks@gmail.com>
2014-08-19 16:32:06 +0700

    Home

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,6 +4,69 @@ class HomeController extends BaseController {
 
 	public function view_index()
 	{
-		return View::make('pages.home');
+		//bikin variable
+		$deskripsi_selamat_datang = '<p>Dramatically visualize customer directed convergence without revolutionary ROI. Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>';
+		
+		$tentang_hfi = '<p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>';
+		
+		$visi_hfi = '<ul>
+						<li>
+							satu 
+						</li>
+						<li>
+							dua 
+						</li>
+						<li>
+							tiga 
+						</li>
+						<li>
+							empat
+						</li>
+						<li>
+							lima 
+						</li>
+						<li>
+							enam 
+						</li>
+						<li>
+							tujuh
+						</li>
+					</ul>';
+		
+		$misi_hfi = '<ul>
+						<li>
+							satu 
+						</li>
+						<li>
+							dua 
+						</li>
+						<li>
+							tiga 
+						</li>
+						<li>
+							empat
+						</li>
+						<li>
+							lima 
+						</li>
+						<li>
+							enam 
+						</li>
+						<li>
+							tujuh
+						</li>
+					</ul>';
+		
+		return View::make('pages.home', compact('deskripsi_selamat_datang', 'tentang_hfi', 'visi_hfi', 'misi_hfi'));
+		
+		//return View::make('pages.home', 
+		//	array(
+		//	'deskripsi_selamat_datang' => $getfromdbdesc
+		//	) 
+		//);
 	}
+	
+	
+	
+	
 }

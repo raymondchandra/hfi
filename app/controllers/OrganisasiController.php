7ea17b06c7c458395e6ae2b212285ce73ac5189e
Jeffry <stephanus_jeffry@yahoo.com>
2014-08-21 18:04:30 +0700

    back end cabang halaman depan

--- app/controllers/OrganisasiController.php
+++ app/controllers/OrganisasiController.php
@@ -14,9 +14,22 @@ class OrganisasiController extends BaseController {
 	public function view_cabang()
 	{
 		$arr = $this->setHeader();
-		return View::make('pages.cabang', compact('arr'));
+		$arr2 = $this->get_semua_cabang();
+		return View::make('pages.cabang', compact('arr','arr2'));
 	}
 
+	public function get_semua_cabang()
+	{
+		$count = Cabang::where('tipe','=', '0')->get();
+		if(count($count) != 0)
+		{
+			return $count;
+			
+		}else
+		{
+			return "";
+		}
+	}
 	
 	//pengurus
 	//public function get_pengurus()

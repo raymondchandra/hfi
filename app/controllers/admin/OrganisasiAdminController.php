26ae14c93eb0e98dd22f672d50ed68a8d9df6953
Jeffry <stephanus_jeffry@yahoo.com>
2014-08-25 12:37:53 +0700

    all cabang back end

--- app/controllers/admin/OrganisasiAdminController.php
+++ app/controllers/admin/OrganisasiAdminController.php
@@ -1,19 +1,37 @@
 <?php
 
-class OrganisasiAdminController extends BaseController {
-	
-	public $restful = true;
-	
-	public function view_cabang()
-	{
-		return View::make('pages.admin.organisasi.cabang');
-	}
-	
-	public function view_pengurus()
-	{
-		return View::make('pages.admin.organisasi.pengurus');
-	}
+	class OrganisasiAdminController extends BaseController {
+		
+		public $restful = true;
+		
+		public function view_cabang()
+		{
+			$arr = $this->get_semua_cabang();
+			return View::make('pages.admin.organisasi.cabang', compact('arr'));
+		}
+		
+		public function view_pengurus()
+		{
+			return View::make('pages.admin.organisasi.pengurus');
+		}
 
-}
+		public function get_semua_cabang()
+		{
+			$count = Cabang::all();
+			if(count($count) != 0)
+			{
+				return $count;
+			}else
+			{
+				return "";
+			}
+		}
+		
+		
+		public function edit_cabang()
+		{
+			
+		}
+	}
 
 ?>

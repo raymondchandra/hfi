62bb19ecd4bef695438249904d715538040166a8
Gredy Prasetya <gredy.prasetya@gmail.com>
2014-08-25 12:19:56 +0700

    Kegiatan

--- app/views/pages/kegiatan.blade.php
+++ app/views/pages/kegiatan.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-	im on kegiatan page
+<div class="grid_12">
+	<div class="main_content">
+		<div>
+			Filter
+		</div>
+		
+		<div>
+			<h2>Kegiatan</h2>
+		</div>
+	</div>
+</div>
+
 @stop

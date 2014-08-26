15c169d164594bdf3af94115e8334839c68ec368
Jeffry <stephanus_jeffry@yahoo.com>
2014-08-26 12:05:29 +0700

    back end cabang admin

--- app/controllers/admin/OrganisasiAdminController.php
+++ app/controllers/admin/OrganisasiAdminController.php
@@ -30,7 +30,47 @@
 		
 		public function edit_cabang()
 		{
-			
+			$id_cabang = Input::get('id_cabang');
+			$cabang = Cabang::find($id_cabang);
+			$cabang->nama = Input::get('nama_cabang');
+			$cabang->telp = Input::get('telp_cabang');
+			$cabang->fax = Input::get('fax_cabang');
+			$cabang->email = Input::get('email_cabang');
+			$cabang->link = Input::get('link_cabang');
+			$cabang->alamat = Input::get('alamat_cabang');
+			$cabang->timestamps = false;
+				
+			$cabang->save();
+			return "Success Update";
+		}
+		
+		public function get_satu_cabang()
+		{
+			$id_cabang = Input::get('id_cabang');
+			$count = Cabang::where('id','=', $id_cabang)->get();
+			if(count($count) != 0)
+			{
+				return $count;
+				
+			}else
+			{
+				return "Fail";
+			}
+		}
+		
+		public function tambah_cabang()
+		{
+			$cabang = new Cabang();
+			$cabang->nama = Input::get('nama_cabang');
+			$cabang->telp = Input::get('telp_cabang');
+			$cabang->fax = Input::get('fax_cabang');
+			$cabang->email = Input::get('email_cabang');
+			$cabang->link = Input::get('link_cabang');
+			$cabang->alamat = Input::get('alamat_cabang');
+			$cabang->timestamps = false;
+				
+			$cabang->save();
+			return "Success Update";
 		}
 	}
 

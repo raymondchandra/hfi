782256e16444ffa62661897b558d4637f63e38a5
Gredy Prasetya <gredy.prasetya@gmail.com>
2014-08-27 10:43:12 +0700

    update slide show admin

--- app/views/pages/admin/home/slideshow.blade.php
+++ app/views/pages/admin/home/slideshow.blade.php
@@ -3,19 +3,19 @@
 <div class='slide_container'>
 	<ul>
 		<li>
-			<div><img src="#" class='slide_image' /> <input type='button' value='Ganti Foto' class='change_photo'/></div><input type='text' placeholder='caption 1'> <input type='button' /><input type='button' />
+			<div><img src="#" class='slide_image' /> <input type='button' value='Ganti Foto' class='change_photo'/></div><input type='text' class='caption' placeholder='caption 1' /><input type='button' class='ok_change' value='Ok' style='display:none;' />
 		</li>
 		<li>
-			<div><img src="#" class='slide_image' /> <input type='button' value='Ganti Foto' class='change_photo'/> <input type='text' placeholder='caption 2'> <input type='button' /><input type='button' />
+			<div><img src="#" class='slide_image' /> <input type='button' value='Ganti Foto' class='change_photo'/> <input type='text' class='caption' placeholder='caption 2' /><input type='button' class='ok_change' value='Ok' style='display:none;' />
 		</li>
 		<li>
-			<div><img src="#" class='slide_image' /> <input type='button' value='Ganti Foto' class='change_photo'/> <input type='text' placeholder='caption 3'> <input type='button' /><input type='button' />
+			<div><img src="#" class='slide_image' /> <input type='button' value='Ganti Foto' class='change_photo'/> <input type='text' class='caption' placeholder='caption 3' /><input type='button' class='ok_change' value='Ok' style='display:none;' />
 		</li>
 		<li>
-			<div><img src="#" class='slide_image' /> <input type='button' value='Ganti Foto' class='change_photo'/> <input type='text' placeholder='caption 4'> <input type='button' /><input type='button' />
+			<div><img src="#" class='slide_image' /> <input type='button' value='Ganti Foto' class='change_photo'/> <input type='text' class='caption' placeholder='caption 4' /><input type='button' class='ok_change' value='Ok' style='display:none;' />
 		</li>
 		<li>
-			<div><img src="#" class='slide_image' /> <input type='button' value='Ganti Foto' class='change_photo'/> <input type='text' placeholder='caption 5'> <input type='button' /><input type='button' />
+			<div><img src="#" class='slide_image' /> <input type='button' value='Ganti Foto' class='change_photo'/> <input type='text' class='caption' placeholder='caption 5' /><input type='button' class='ok_change' value='Ok' style='display:none;' />
 		</li>
 	</ul>
 </div>
@@ -23,6 +23,16 @@
 
 
 <script>
+
+$('.caption').keyup(function(){
+	$(this).next().removeAttr('style');
+});
+
+$('.ok_change').click(function(){
+	$(this).css('display','none');
+	alert($(this).prev().val());
+});
+
 $('body').on('mouseenter','.slide_image',function(){
 	$('.change_photo').fadeIn(10);
 });

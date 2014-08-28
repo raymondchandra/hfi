4829d35b3cb699ef5803c467c492d710ad30e236
domiworks <domiworks@gmail.com>
2014-08-28 09:44:47 +0700

    Halaman ubah password (changepassword.blade.php) dan penambahan link di halaman profile

--- app/routes.php
+++ app/routes.php
@@ -29,6 +29,7 @@ Route::get('/kontak', ['as' => 'kontak', 'uses' => 'KontakController@view_index'
 Route::get('/login', ['as' => 'login', 'uses' => 'AccountController@view_login', 'before' => 'checkLogin']);
 Route::get('/registrasi', ['as' => 'registrasi', 'uses' => 'AccountController@view_registrasi']);
 Route::get('/forgotpassword', ['as' => 'forgotpassword', 'uses' => 'AccountController@view_forgotpassword']);
+Route::get('/ubahpassword', ['as' => 'changepassword', 'uses' => 'AccountController@view_changepassword']);
 
 //get daftar cabang buat registrasi	
 Route::get('/registrasi/daftarcabang', ['as' => 'registrasi.daftarcabang', 'uses' => 'AccountController@view_registrasi']);
---
+++ app/views/pages/changepassword.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.default')
+@section('content')
+	<div class="grid_4 push_4">
+		<div class="login_content">
+			<div class="table">
+				<div class="table_cell">
+
+
+						
+						<div class="login_container">
+							<div class="alert_wrong_pass">
+								@if(Session::has('message'))
+									<p class="alert" style="margin: 0px !important;">
+										Username atau email anda tidak terdaftar
+									</p>
+								@endif
+							</div>
+							
+							<h1 class="logintitle">
+								Lupa Password
+							</h1>
+							<div class="form">
+								{{ Form::open(array('url' => '/signin')) }}
+								<form>
+									<!--<input type="text"  placeholder="Username"/>-->
+									{{ Form::password('oldpassword', Input::old('oldpassword')) }}
+									<!--<input type="password" placeholder="Password"/>-->
+									{{ Form::password('newpassword', Input::old('newpassword')) }}
+									
+									{{ Form::password('retypenewpassword', Input::old('retypenewpassword')) }}
+									<!--<input type="button" value="Log In" class="login"/>-->
+									<div style="display: block; position: relative; width: 100$; height: 30px; overflow: hidden;">
+										{{ Form::submit('Ubah Password', array('class' => 'login')) }}
+									</div>
+								</form>								 
+								{{ Form::token() }}
+								{{ Form::close() }}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@stop

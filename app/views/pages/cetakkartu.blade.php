1f5a2ab413955f4bae84c91f48cda16a9eef7f3a
domiworks <domiworks@gmail.com>
2014-08-31 18:47:40 +0700

    Kartu HFI 48%

---
+++ app/views/pages/cetakkartu.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.print')
+@section('content')	
+
+	<div class="kartu_size">
+		<div class="left">
+		
+			<div class="head_kartu_title">
+				<span class="title">
+					Member
+				</span>
+				<span class="tahun">
+					2014
+				</span>
+			</div>
+			<div class="nama_member">
+				Nama Seseorang
+			</div>
+			<div class="big_footer_area">
+				<div class="left">
+					<div class="no_and_section">
+						<span class="member_no">
+							Member #23456
+						</span>
+						<span class="regional">
+							Bandung, Jawa Barat
+						</span>
+					</div>
+					<div class="valid_waktu">
+						Valid for 3 years
+					
+					</div>
+					<span class="vlag_kecil">
+					</span>
+					<div class="website">
+						http://hfi.fisika.net/
+					</div>
+				</div>
+				<div class="right">
+					<div class="tanda_tangan_pres">
+						qwertyu
+					</div>
+					<div class="logo_hfi">
+						qwertyu
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		
+		<div class="right">
+		
+		</div>
+	</div>
+	
+@stop

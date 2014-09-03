6a4d9ce72615d09275fd58ab748c80adf9778997
Raymond Chandra <ray_flare@yahoo.com>
2014-09-03 09:35:37 +0700

    kegiatan, database migrate, seed

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,48 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		// $this->call('UserTableSeeder');
+
+		$acc = new Account();
+		$acc->timestamps = false;
+	    $acc->username = 'ketua';
+	    $acc->password = Hash::make('ketua');
+		$acc->status_aktif = 1;
+		$acc->role = 1;
+	    $acc->save();
+		
+		$acc = new Account();
+		$acc->timestamps = false;
+	    $acc->username = 'wakil';
+	    $acc->password = Hash::make('wakil');
+		$acc->status_aktif = 1;
+		$acc->role = 1;
+	    $acc->save();
+		
+		$acc = new Account();
+		$acc->timestamps = false;
+	    $acc->username = 'sekertaris';
+	    $acc->password = Hash::make('sekertaris');
+		$acc->status_aktif = 1;
+		$acc->role = 1;
+	    $acc->save();
+		
+		$acc = new Account();
+		$acc->timestamps = false;
+	    $acc->username = 'bendahara';
+	    $acc->password = Hash::make('bendahara');
+		$acc->status_aktif = 1;
+		$acc->role = 1;
+	    $acc->save();
+		
+		$cabang = new Cabang();
+		$cabang->nama = 'Pusat';
+		$cabang->telp = 123;
+		$cabang->fax = 123;
+		$cabang->email = 'asd@asd.com';
+		$cabang->link = 'www.asd.com';
+		$cabang->alamat = 'asdf';
+		$cabang->timestamps = false;
+		$cabang->save();
 	}
 
 }
---
+++ app/models/Kegiatan.php
@@ -0,0 +1,7 @@
+<?php
+
+class Kegiatan extends Eloquent
+{
+	protected $table = 'kegiatan';
+	
+}

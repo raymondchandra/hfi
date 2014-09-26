621cec834de139956cc92c61763410405b9f751e
domiworksPC <domiworks@gmail.com>
2014-09-26 14:32:52 +0700

    -akun admin aneh previous next [v] -akun admin tombol reset button [v] -regulasi x button mepet, trus x ny ganti [v] -sidebar kegiatan panah admin panel ga gerak [v] -registrasi cm boleh guest -tabel pengurus samain ky yg lain -regulasi validasi css error -pengurus validasi error css -jenis publikasi admin rubah -aturan jd anggota ubah ketengahin - e berkas tabel samain [v] -tambah berkas scroll aneh [v] -berkas edit aneh [v] -berkas button blm diganti [v]

--- app/views/pages/admin/home/regulasi.blade.php
+++ app/views/pages/admin/home/regulasi.blade.php
@@ -38,9 +38,9 @@
 								for($i=0; $i<length; $i++){
 									arrIDRegulasi[$i] = data[$i]['id'];
 									arrFILEPATHRegulasi[$i] = data[$i]['file_path'];
-									list+="<tr>";							
-									list+="<td><a href='javascript:void(0)' class='versi_regulasi' value='"+data[$i]['file_path']+"'>"+data[$i]['versi']+"</a></td>";
-									list+="<td><p style='display:none;'>"+data[$i]['file_path']+"</p><input type='button' value='x' class='hapus_regulasi'/><input type='hidden' class='id_regulasi' value='"+$i+"'/> </td>";
+									list+="<tr style='padding-top:5px; padding-bottom: 5px; '>";							
+									list+="<td><a href='javascript:void(0)' class='versi_regulasi' style='line-height: 36px; margin-right: 10px;' value='"+data[$i]['file_path']+"'>"+data[$i]['versi']+"</a></td>";
+									list+="<td><p style='display:none;'>"+data[$i]['file_path']+"</p><input type='button' value='hapus' class='hapus_regulasi btn btn-danger'/><input type='hidden' class='id_regulasi' value='"+$i+"'/> </td>";
 									list+="</tr>";
 								}
 								$('.tabel_list_regulasi').html(list);

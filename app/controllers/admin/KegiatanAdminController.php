6375c913a7e2d262ea9fd21d8f07fae9e848ba49
Raymond Chandra <ray_flare@yahoo.com>
2014-09-26 13:02:22 +0700

    kegiatan admin

--- app/controllers/admin/KegiatanAdminController.php
+++ app/controllers/admin/KegiatanAdminController.php
@@ -42,7 +42,33 @@ class KegiatanAdminController extends BaseController {
 		$kegiatan->timestamps = false;
 		try {
 			$kegiatan->save();
-			return "Success Insert";
+			if(Input::hasFile('photo'))
+			{
+				$img_upload = Input::file('photo');
+				$file_name = $img_upload->getClientOriginalName();
+				
+				if($kegiatan == NULL)
+				{
+					//error
+					return "Gagal Update Foto";
+				}
+				else
+				{
+					$pathLama = $kegiatan -> brosur_kegiatan;
+					if($pathLama != NULL)
+					{
+						File::delete($pathLama);
+						$destination = 'assets/file_upload/kegiatan/'.$kegiatan->id;
+					}
+					$uploadSuccess   = $img_upload->move($destination, $file_name);
+					$kegiatan->brosur_kegiatan = $destination.$file_name;
+					$kegiatan->save();
+					return "Success Insert";
+				}
+			}else
+			{
+				return "Gagal Update Foto";
+			}
 		} catch (Exception $e) {
     		return 'Caught exception: '. $e->getMessage(). "\n";
 		}
@@ -53,12 +79,38 @@ class KegiatanAdminController extends BaseController {
 		$id_kegiatan = Input::get('id_kegiatan');
 		$kegiatan = Kegiatan::find($id_kegiatan);
 		if($kegiatan->type == 1 || $kegiatan->type == 2){
+			if(Input::hasFile('photo'))
+			{
+				$img_upload = Input::file('photo');
+				$file_name = $img_upload->getClientOriginalName();
+				
+				if($kegiatan == NULL)
+				{
+					//error
+					return "Gagal Update Foto";
+				}
+				else
+				{
+					$pathLama = $kegiatan -> brosur_kegiatan;
+					if($pathLama != NULL)
+					{
+						File::delete($pathLama);
+						$destination = 'assets/file_upload/kegiatan/'.$kegiatan->id;
+					}
+					$uploadSuccess   = $img_upload->move($destination, $file_name);
+					$kegiatan->brosur_kegiatan = $destination.$file_name;
+				}
+			}else
+			{
+				return "Gagal Update Foto";
+			}
+
 			$kegiatan->nama_kegiatan = Input::get('nama_kegiatan');
 			$kegiatan->tempat = Input::get('tempat');
 			$datepiece = explode('.',Input::get('tanggal_mulai'));
-			$date_start = $datepiece[2].'-'.$datepiece[1].'-'.$datepiece[0].' '.Input::get('waktu_mulai').':00'.;
+			$date_start = $datepiece[2].'-'.$datepiece[1].'-'.$datepiece[0].' '.Input::get('waktu_mulai').':00';
 			$datepiece = explode('.',Input::get('tanggal_selesai'));
-			$date_finish = $datepiece[2].'-'.$datepiece[1].'-'.$datepiece[0].' '.Input::get('waktu_selesai').':00'.;
+			$date_finish = $datepiece[2].'-'.$datepiece[1].'-'.$datepiece[0].' '.Input::get('waktu_selesai').':00';
 			$kegiatan->waktu_mulai = $date_start;
 			$kegiatan->waktu_selesai = $date_finish;
 			$kegiatan->deskripsi = Input::get('deskripsi');

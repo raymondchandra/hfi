c5cabb2448f6e74268d6e87ffc42af131a7e5b0a
domiworksNOTEBOOK <domiworks@gmail.com>
2014-09-29 04:46:17 +0700

    Simposium route sementara: • /test/simposium • /test/simposium_tanggal • /test/simposium_lokasi • /test/simposium_registrasi • /test/simposium_peserta

---
+++ app/views/pages/simposium_main.blade.php
@@ -0,0 +1,133 @@
+@extends('layouts.simposium')
+@section('content')
+<div style="width: 100%; background: #ededed;">
+<div class="container_12" >	
+	<div class="grid_12">
+		<img src="{{ asset('assets/img/simposium_header.jpg') }}" width="940" alt="simpsium hfi"/>
+
+	</div>
+</div>
+</div>
+
+<div class="container_12">
+	<div class="grid_12 s_sponsor_area">
+		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+
+	</div>
+</div>
+<div class="container_12">
+	<div class="grid_12">
+		<div class="main_content">
+			<div class="side_panel_hfi">
+				<div class="side_panel_hfi_background">
+				<ul>
+					<li>
+						<a href="javascript:void(0)">Pertemuan Lalu</a>
+					</li>
+					<span class="white_space">&nbsp;</span>
+					<li>
+						<a href="">Tanggal Penting</a>
+					</li>
+					<span class="white_space">&nbsp;</span>
+					<li>
+						<a href="">
+							Lokasi
+						</a>
+					</li>
+					<span class="white_space">&nbsp;</span>
+					<li>
+						<a href="">
+							Akomodasi
+						</a>
+					</li>
+					<span class="white_space">&nbsp;</span>
+					<li>
+						<a href="">
+							Program
+						</a>
+					</li>
+					<span class="white_space">&nbsp;</span>
+					<li>
+						<a href="">
+							Registrasi
+						</a>
+					</li>
+					<span class="white_space">&nbsp;</span>
+					<li>
+						<a href="">
+							Peserta
+						</a>
+					</li>
+					<span class="white_space">&nbsp;</span>
+					<li>
+						<a href="">
+							Prosiding
+						</a>
+					</li>
+					<span class="white_space">&nbsp;</span>
+					<li>
+						<a href="">
+							Panitia
+						</a>
+					</li>
+					<span class="white_space">&nbsp;</span>
+					<li>
+						<a href="">
+							Kontak
+						</a>
+					</li>
+				
+				</ul>
+				</div>
+			</div>
+			
+			<div class="content_hfi">
+				<h1 style="text-align: center;">In Conjunction with The 4th International Conference on Theoretical and Applied Physics (ICTAP)</h1>
+				<hr>
+				</hr>
+				<h2>
+					Undangan Simposium
+				</h2>
+				<p>
+					Merupakan suatu kehormatan bagi Jurusan Fisika FMIPA Universitas Udayana yang telah dipercaya oleh Himpunan Fisika Indonesia (HFI) untuk menyelenggarakan Simposium Fisika Nasional SFN XXVII yang akan dilaksanakan pada tanggal 16-17 Oktober 2014 di Kampus Pascasarjana Universitas Udayana, Jl. P.B. Sudirman, Denpasar-Bali. Simposium bertujuan untuk menyebarluaskan hasil-hasil penelitian dan mendiskusikan isu-isu terkini dari berbagai bidang disiplin ilmu dalam Fisika.
+
+					Peneliti dari universitas, institut dan industri terkait dalam berbagai bidang disiplin Ilmu Fisika diundang untuk berpartisipasi untuk mempresentasikan hasil penelitiannya atau hanya sebagai peserta atau pengamat. Hasil penelitian yang dipresentasikan akan dimuat dalam prosiding Simposium Fisika Nasional SFN XXVII atau dimuat dalam jurnal Himpunan Fisika Indonesia setelah melalui beberapa tahap seleksi.
+
+					Ruang lingkup seminar mencakup beberapa bidang disiplin Ilmu Fisika, diantaranya : Fisika Teori dan Komputasi, Material Maju dan Nanoteknologi, Biofisika dan Medis, Fisika Nuklir, Geofisika, Astrofisika, Instrumentasi, Laser dan Optoelektronika, Fisika Energi dan Lingkungan, dan Fisika Pendidikan.
+				</p>
+				<hr>
+				</hr>
+				<h2>
+					Template
+				</h2>
+				<p>
+					1. Template untuk abstrak SFN 2014 dapat diunduh di <a href"http://opi.lipi.go.id/data/1082785832/data/1407071304.zip">Template Abstrak SFN XVII 2014</a> <br/>
+					2. Template untuk prosiding SFN 2014 dapat diunduh di <a href="http://opi.lipi.go.id/data/1082785832/data/1407071104.zip">Template Prosiding SFN XVII 2014</a>
+				</p>
+				<hr>
+				</hr>
+				<h2>
+					Pembicara Utama
+				</h2>
+				<p>
+					Prof. Hermawan K. Dipojono, Ph.D (DIKTI, INA)<br/>
+					Dr. Bambang Widyatmoko (LIPI, INA)<br/>
+					Prof. Dr. Halmar Halide (UNHAS, INA)<br/>
+					Dr. Bobby Eka Gunara (ITB, INA)<br/>
+					Dr. Hery Suyanto (UNUD, INA)<br/>
+					Dr. Ida Kaniawati (UPI, INA)
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+@stop

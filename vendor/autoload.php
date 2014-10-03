5573c85bbeeaf888149e9fd32f757a7d2bb44613
Gredy Prasetya <gredy.prasetya@gmail.com>
2014-10-03 21:35:28 +0700

    autoload

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit15180ce3ae6896025696ba0b6987a92f::getLoader();
+return ComposerAutoloaderInitc0dd5a4a2d4169635150458482312d13::getLoader();

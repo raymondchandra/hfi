350721b6f12ed2c77d1c5269fde5b01e7c13e9d0
domiworksPC <domiworks@gmail.com>
2014-10-03 15:22:04 +0700

    Simposium Admin: • Umum • Header dan Sponsor

--- app/views/includes/simposium/sponsor.blade.php
+++ app/views/includes/simposium/sponsor.blade.php
@@ -1,13 +1,13 @@
 <div class="container_12">
 	<div class="grid_12 s_sponsor_area">
-		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
-		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
-		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
-		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
-		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
-		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
-		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
-		<img src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img class="sponsor_img" src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img class="sponsor_img" src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img class="sponsor_img" src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img class="sponsor_img" src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img class="sponsor_img" src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img class="sponsor_img" src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img class="sponsor_img" src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
+		<img class="sponsor_img" src="{{ asset('assets/img/sponsor.jpg') }}" width="64" alt="simpsium hfi"/>
 
 	</div>
 </div>
--- app/views/pages/simposium/admin/simposium_sponsor.blade.php
+++ app/views/pages/simposium/admin/simposium_sponsor.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts.simposium_admin')
+@section('content')
 <script>
 	var id = '{{$id}}';
 </script>
@@ -14,6 +16,46 @@
 			<img src="" alt="foto2">
 		</div>
 
+		<div class="form-group">
+			<label class=" control-label col-sm-3">Pendidikan<span class="red">*</span></label>
+			<script>
+				var lastIdx = 2;
+				function addPendidikan(){
+					//if(lastIdx <=5)
+					//{
+						var newRow = "<div class='clearfix'></div>";
+						newRow += "<div id='divPendidikan"+lastIdx+"' style='margin-top: 5px;' '>";
+						newRow +="<input type='file' id='pendidikan"+lastIdx+"' name='pendidikan"+lastIdx+"' class='texPendidikan form-control col-sm-3 col-sm-push-3' />";
+						newRow +="<span value='' id='editPendidikan"+lastIdx+"' onClick='' style='padding: 0px; width: 34px; line-height: 34px; margin-left:10px;' class='glyphicon glyphicon-edit form-control col-sm-push-3 btn btn-warning'></span>";
+						newRow +="<span value='' id='editPendidikan"+lastIdx+"' onClick='' style='padding: 0px; width: 34px; line-height: 34px; margin-left:10px;' class='glyphicon glyphicon-remove form-control col-sm-push-3 btn btn-danger'></span>";
+						newRow +="<span value='' id='delPendidikan"+lastIdx+"' onClick='delPendidikan()' style='padding: 0px; width: 34px; line-height: 34px; margin-left:10px;' class='glyphicon glyphicon-remove form-control col-sm-push-3 btn btn-danger'></span><br /></div>";
+						$('#delPendidikan'+(lastIdx-1)).hide();
+						$('#addPendidikan').append(newRow);
+					//	if(lastIdx==5){
+					//		$('#refPendidikan').hide();
+					//	}
+						lastIdx++;
+						
+				//	}
+					
+				}
+				function delPendidikan()
+				{
+					$('#divPendidikan'+(lastIdx-1)).remove();
+					lastIdx--;
+					$('#delPendidikan'+(lastIdx-1)).show();
+					//if(lastIdx==5){
+				//		$('#refPendidikan').show();
+				//	}
+				}
+			</script>
+				<input type="file" id="pendidikan1" name="pendidikan1" class='texPendidikan form-control col-sm-3' style=""/>
+				
+				<a href="javascript:void(0)" onClick = "addPendidikan();" id="refPendidikan" class="glyphicon glyphicon-plus btn btn-primary" style="padding: 0px;width: 34px; line-height: 34px; margin-left:10px;"></a>
+			
+			<div id="addPendidikan"></div>
+		</div>	
+
 		<div id="uploadttg" style="margin-left: 20px !important;">
 			<form id="form_edit_tanda_tangan">
 				<img id="gambar_tanda_tangan" src="" alt="tandatangan" width="118" height="63"/>
@@ -23,3 +65,5 @@
 		</div>
 	</div>
 </div>
+
+@stop
